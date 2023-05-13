063e29656a2976d394c057b92b2cffee1db416d4
amirahmadadibi <amirahmadadibi@gmail.com>
2023-05-13 20:07:44 +0330

    S08 E02 create note input layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          body: ListView.builder(
-        itemCount: 300,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.only(bottom: 10),
-            height: 100,
-            color: Colors.red,
-            child: Text('7Learn'),
-          );
-        },
-      )),
-    );
+        home: Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(width: 3, color: Colors.red),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(width: 3, color: Colors.green),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('افزودن'))
+          ],
+        ),
+      ),
+    )
+
+        //        ListView.builder(
+        //     itemCount: 300,
+        //     itemBuilder: (context, index) {
+        //       return Container(
+        //         margin: EdgeInsets.only(bottom: 10),
+        //         height: 100,
+        //         color: Colors.red,
+        //         child: Text('7Learn'),
+        //       );
+        //     },
+        //   )
+        );
   }
 }
